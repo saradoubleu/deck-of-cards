fac892f881c585528e0922fb8ed3ea724d31de7b
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-10-01 19:45:21 -0400

    feat: shuffle()

--- src/components/CardDeck/CardDeck.js
+++ src/components/CardDeck/CardDeck.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Card from "../Card/Card";
+import {
+  cardSuite,
+  cardRank,
+  standardCardDeckGenerator,
+} from "../../data/data";
 
-const CardDeck = (props) => {
-  const handleClick = (event) => {
-    event.preventDefalt();
+const CardDeck = () => {
+  //   let currentDeck = [];
+  let standardDeck = standardCardDeckGenerator(cardSuite, cardRank);
+
+  //Durstenfeld / Knuth Shuffling Algo
+  const shuffle = (cards) => {
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+    console.log(cards);
   };
+
+  console.log("shuffle: ", shuffle(standardDeck));
 
   return (
     <div className="card-deck-wrapper">
-      {/* Hello{" "} */}
-      <Card onClick={(e) => this.handleClick(e)}>
-        {props.cards.map((el) => el)}
-      </Card>
+      <Card />
+      {standardDeck.map((el) => (
+        <li key={el.toString()}>{el}</li>
+      ))}
+      {/* {standardCardDeckGenerator} */}
     </div>
   );
 };
